0a1cb090442980aefb02a5593b85abd644d2a99b
vbarzana <viticoinf@gmail.com>
2023-03-10 12:17:31 +0200

    fix: solved issue with sequelize not working as expected

---
+++ src/config/blueprints.js
@@ -0,0 +1,7 @@
+module.exports = {
+  /**
+   * @property {number} defaultLimit Default limit property can be used anytime to always
+   * paginate at the api level so we don't end up with memory leaks per instance
+   */
+  defaultLimit: 25,
+};
